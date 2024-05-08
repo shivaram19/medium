39ceb47801a3b20e7fc56c3ce1985f97749141bc
ShivaramMorigala <shivaramiitmb085@gmail.com>
2024-05-08 19:56:02 +0530

    added jwt_secret in wrangler.toml

---
+++ backend/prisma/migrations/20240508112546_init_schema/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "pubmished" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" TEXT NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Post_title_key" ON "Post"("title");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,32 +1,63 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import dotenv from "dotenv"
+import { decode, sign, verify } from 'hono/jwt'
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL: string,
+    JWT_SECRET: string
+  }
+}>();
+dotenv.config()
 
-const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.post('api/v1/user/signup',(req, res) => {
-  return 
+app.post('api/v1/user/signup',async (c) => {
+
+    const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json()
+  const user = await prisma.user.create({
+    data: {
+      email: body.email, 
+      password: body.password,
+      username: body.username
+    }
+  })
+
+  const token = await sign({ id: user.id }, c.env.JWT_SECRET )
+
+  return c.json({
+    jwt: token
+  })
+
 })
 
-app.post('/api/v1/user/signin',(){
-
+app.post('/api/v1/user/signin',(c) => {
+  return c.text("hello world")
 })
 
-app.post('/api/v1/blog',(){
-
+app.post('/api/v1/blog',(c) => {
+  return c.text("hello world")
 })
 
-app.put('/api/v1/blog',(){
-  
+app.put('/api/v1/blog',(c) => {
+  return c.text("hello world")
 })
 
-app.get('/api/v1/blog/:id',{
-
+app.get('/api/v1/blog/:id',(c) => {
+  return c.text("hello world")
 })
 
-app.get('api/v1/blog/bulk',(){
-
+app.get('api/v1/blog/bulk',(c) => {
+  return c.text("hello world")
 })
+
+
 export default app
