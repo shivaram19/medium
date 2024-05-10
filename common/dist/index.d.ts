fa3e9ee0b827d8199d26feea7511ada60eb0e042
ShivaramMorigala <shivaramiitmb085@gmail.com>
2024-05-10 22:52:36 +0530

    zod

---
+++ common/dist/index.d.ts
@@ -0,0 +1,48 @@
+import z from "zod";
+export declare const signUpInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+}, {
+    email: string;
+    password: string;
+}>;
+export declare const signInInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+}, {
+    email: string;
+    password: string;
+}>;
+export declare const createBlogInput: z.ZodObject<{
+    title: z.ZodString;
+    content: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    title: string;
+    content: string;
+}, {
+    title: string;
+    content: string;
+}>;
+export declare const updateBlogInput: z.ZodObject<{
+    title: z.ZodString;
+    content: z.ZodString;
+    id: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    title: string;
+    content: string;
+    id: string;
+}, {
+    title: string;
+    content: string;
+    id: string;
+}>;
+export type SignInInput = z.infer<typeof signInInput>;
+export type SignUpInput = z.infer<typeof signUpInput>;
+export type CreateBlogInput = z.infer<typeof createBlogInput>;
+export type UpdateBlogInput = z.infer<typeof updateBlogInput>;
